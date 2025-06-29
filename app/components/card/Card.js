0124feefff6ce3d793510970ab28d045afa56fca
iuvasd <chatgptmachine1@proton.me>
2025-06-30 00:14:30 +0300

    Add files via upload

--- app/components/card/Card.js
+++ app/components/card/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./card.module.css";
 
-const Card = ({ type, data }) => {
+const Card = ({ type, data, servicesPage }) => {
   if (type === "howItWorks") {
     return (
       <div className={`${styles.card} ${styles.howItWorksCard}`} style={{ maxWidth: data.width ? data.width : "", width: "100%", textAlign: data.textAlign ? "center" : "" }}>
@@ -30,7 +30,7 @@ const Card = ({ type, data }) => {
     return (
       <div className={`${styles.card} ${styles.servicesCard}`}>
         <div className={styles.cardTop}>
-          <div className={styles.cardImgWrapper} style={{ backgroundColor: data.background }}>
+          <div className={`${styles.cardImgWrapper} ${servicesPage ? styles.cardImgWrapperService : undefined}`} style={{ backgroundColor: data.background }}>
             <img style={{ width: data.width }} src={data.img} alt="" />
           </div>
           <div style={{ textAlign: "center" }}>
@@ -41,14 +41,19 @@ const Card = ({ type, data }) => {
         </div>
         <div className={styles.cardTitle}>{data.name}</div>
         <div className={styles.cardText}>{data.title}</div>
-        <div className={styles.cardInfo}>{data.info}</div>
+        <div className={`${styles.cardInfo} ${servicesPage ? styles.serviceP : undefined}`}>{data.info}</div>
         <button onClick={data.onclick}>
           Ознакомиться детальнее
-          <svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.5 1L8.83744 7.84828C8.9243 7.92935 8.92175 8.06782 8.83197 8.14563L1.5 14.5" stroke="white" strokeWidth="2" strokeLinecap="round" />
-          </svg>
+          {
+            !servicesPage && <svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.5 1L8.83744 7.84828C8.9243 7.92935 8.92175 8.06782 8.83197 8.14563L1.5 14.5" stroke="white" strokeWidth="2" strokeLinecap="round" />
+            </svg>
+          }
         </button>
-      </div>
+        {
+          servicesPage && <button className={styles.servicesCardBUtton}>Приобрести сервис</button>
+        }
+      </div >
     );
   }
 
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,34 @@
+"use client"
+
+import Footer from "./components/footer";
 import "./globals.css";
+import styles from "./page.module.css"
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>AbbsNet - Быстрый, безопасный и недорогой доступ к сервисам и нейросетям</title>
-        <meta name="description" content="С AbbsNet вы сможете легко экономить до 95% средств на сервисах, без ограничений, блокировок и других проблем." />
+        <title>ABBSNET</title>
+        <meta name="description" content="Быстрый, безопасный и недорогой доступ к сервисам и нейросетям." />
         <meta name="keywords" content="доступ, сервисы, нейросети, безопасный доступ, быстрый доступ, недорогой доступ" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
+        <div className={styles.banner}>
+          <h3><img src="/assets/money.png" alt="" /> Начни экономить от <span>2000₽</span> в месяц уже сегодня!</h3>
+        </div>
+        <div className={styles.headerTop}>
+          <p>Скачать браузер для экономии</p>
+        </div>
         <main>{children}</main>
+        <Footer className={pathname === "/download" ? styles.footerDownload : ""} />
       </body>
     </html>
   );
