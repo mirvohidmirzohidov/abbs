05a2faab68d5146394cc80c4cc475775c4b8fb2e
iuvasd <chatgptmachine1@proton.me>
2025-09-17 23:26:08 +0300

    Update page.js

--- app/services/freepik/page.js
+++ app/services/freepik/page.js
@@ -60,8 +60,8 @@ const Page = () => {
               <div style={{ gap: "5px" }} className={styles.flex}>
                 <div>
                   <div className={styles.discount}><span>-70%</span></div>
-                  <p className={styles.price}>699₽</p>
-                  <p className={styles.oldPrice}>7000₽</p>
+                  <p className={styles.price}>450₽</p>
+                  <p className={styles.oldPrice}>1400₽</p>
                 </div>
                 <div style={{ backgroundColor: "#2163C7" }} className={styles.imgWrapper}>
                   <img style={{ width: "65%" }} src="/assets/icons/freepik.svg" alt={`Freepik складчина`} />
@@ -86,8 +86,8 @@ const Page = () => {
               </div>
               <div>
                 <div className={styles.discount}><span>-70%</span></div>
-                <p className={styles.price}>699₽</p>
-                <p className={styles.oldPrice}>7000₽</p>
+                <p className={styles.price}>450₽</p>
+                <p className={styles.oldPrice}>1400₽</p>
               </div>
             </div>
             <h1>Купить Freepik Premium аккаунт. Полноценный доступ к подписке</h1>
@@ -139,8 +139,8 @@ const Page = () => {
             <div className={styles.section_right}>
               <div className={styles.section_right_top}>
                 <div className={styles.discount}>-70%</div>
-                <p className={styles.price}>699₽ <span>/ месяц</span></p>
-                <p className={styles.oldPrice}>7000₽</p>
+                <p className={styles.price}>450₽ <span>/ месяц</span></p>
+                <p className={styles.oldPrice}>1400₽</p>
               </div>
               <div className={styles.content}>
                 <div className={styles.info}>
@@ -306,5 +306,6 @@ const Page = () => {
     </>
   )
 }
+
 
 export default Page
