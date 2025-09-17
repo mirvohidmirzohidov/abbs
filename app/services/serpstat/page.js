bef2cc7e5260b6d41f163175b27d7dfe257e68f4
iuvasd <chatgptmachine1@proton.me>
2025-09-18 00:23:16 +0300

    Update page.js

--- app/services/serpstat/page.js
+++ app/services/serpstat/page.js
@@ -55,9 +55,9 @@ const Page = () => {
             <div className={styles.section_right}>
               <div style={{ gap: "5px" }} className={styles.flex}>
                 <div>
-                  <div className={styles.discount}><span>-70%</span></div>
-                  <p className={styles.price}>699₽</p>
-                  <p className={styles.oldPrice}>7000₽</p>
+                  <div className={styles.discount}><span>-85%</span></div>
+                  <p className={styles.price}>1550₽</p>
+                  <p className={styles.oldPrice}>9980₽</p>
                 </div>
                 <div style={{ backgroundColor: "white" }} className={styles.imgWrapper}>
                   <img style={{ width: "60%" }} src="/assets/icons/serpstat.svg" alt={`serpstat складчина`} />
@@ -81,9 +81,9 @@ const Page = () => {
                 <img style={{ width: "60%" }} src="/assets/icons/serpstat.svg" alt={`serpstat складчина`} />
               </div>
               <div>
-                <div className={styles.discount}><span>-70%</span></div>
-                <p className={styles.price}>699₽</p>
-                <p className={styles.oldPrice}>7000₽</p>
+                <div className={styles.discount}><span>-85%</span></div>
+                <p className={styles.price}>1550₽</p>
+                <p className={styles.oldPrice}>9980₽</p>
               </div>
             </div>
             <h1>Купить Serpstat аккаунт. Полноценный доступ к подписке.</h1>
@@ -134,9 +134,9 @@ const Page = () => {
             </div>
             <div className={styles.section_right}>
               <div className={styles.section_right_top}>
-                <div className={styles.discount}>-70%</div>
-                <p className={styles.price}>699₽ <span>/ месяц</span></p>
-                <p className={styles.oldPrice}>7000₽</p>
+                <div className={styles.discount}>-85%</div>
+                <p className={styles.price}>1550₽ <span>/ месяц</span></p>
+                <p className={styles.oldPrice}>9980₽</p>
               </div>
               <div className={styles.content}>
                 <div className={styles.info}>
@@ -222,5 +222,6 @@ const Page = () => {
     </>
   )
 }
+
 
 export default Page
