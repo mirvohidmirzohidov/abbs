77f05ba7103c5c44a4785e915b162d1513a5e407
iuvasd <chatgptmachine1@proton.me>
2025-09-18 18:13:36 +0300

    Update page.js

--- app/services/supa/page.js
+++ app/services/supa/page.js
@@ -179,7 +179,9 @@ const Page = () => {
                     <p className={styles.subtitle}>Доступ к Supa предоставляется через наш браузер AbbsNet.
                       Детальнее ознакомиться с тем, зачем он нужен и как работает Вы можете на нашей <Link style={{ color: "#989899" }} href={"/"}><span>главной странице</span></Link> или посмотрев видео-обзор по кнопке ниже.</p>
                     <div className={styles.buttonItem}>
+                     <a href="https://youtu.be/rU757HthYiU?si=DxjEor-16qVbMyOt">
                       <button>Как работает аккаунт Supa в AbbsNet?</button>
+                      </a>
                     </div>
                     <p className={styles.text}>
                       <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -301,7 +303,7 @@ const Page = () => {
             >
               <div className={`${styles.modalItem} ${!openModal ? styles.modalHide : styles.modalShow}`}>
                 <div className={styles.closeButton}>X</div>
-                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/x2s_Y5ON-ms?si=jkZGHLiAMBBdmwpk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/rU757HthYiU?si=fZgj68OSk4D9JrN_" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
               </div>
             </div>
           )
@@ -313,3 +315,4 @@ const Page = () => {
 
 
 export default Page
+
