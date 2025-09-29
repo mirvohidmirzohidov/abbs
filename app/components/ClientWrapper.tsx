7132898d17f571eab546c18c9a70f705bbd5ae79
Jamoliddin <jamoliddindev@gmail.com>
2025-09-30 01:15:17 +0500

    feat: added register and login buttons in navbar

--- app/components/ClientWrapper.tsx
+++ app/components/ClientWrapper.tsx
@@ -75,7 +75,10 @@ export default function ClientWrapper({ children }: { children: React.ReactNode 
                             Скачать приложение
                         </Link>
                     </div>
-                    <button onClick={() => window.open("https://t.me/m/jidwlHXNNDFi", "_blank")}>Написать нам</button>
+                    <div className={styles.authButtons}>
+                        <button >Вход</button>
+                        <button >Регистрация</button>
+                    </div>
                     <div className={styles.burger_menu}>
                         <svg onClick={() => setOpenMenu(true)} width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" clipRule="evenodd" d="M0.766667 0.601562C0.343252 0.601562 0 0.944814 0 1.36823C0 1.79164 0.343252 2.1349 0.766667 2.1349H19.1667C19.59 2.1349 19.9333 1.79164 19.9333 1.36823C19.9333 0.944814 19.59 0.601562 19.1667 0.601562H0.766667ZM0 7.50156C0 7.07815 0.343252 6.7349 0.766667 6.7349H19.1667C19.59 6.7349 19.9333 7.07815 19.9333 7.50156C19.9333 7.92498 19.59 8.26823 19.1667 8.26823H0.766667C0.343252 8.26823 0 7.92498 0 7.50156ZM0 13.6349C0 13.2115 0.343252 12.8682 0.766667 12.8682H19.1667C19.59 12.8682 19.9333 13.2115 19.9333 13.6349C19.9333 14.0582 19.59 14.4016 19.1667 14.4016H0.766667C0.343252 14.4016 0 14.0582 0 13.6349Z" fill="#F3F3F3" />
@@ -101,7 +104,10 @@ export default function ClientWrapper({ children }: { children: React.ReactNode 
                                         <Link href={"/services"}>Сервисы</Link>
                                         <Link href={"/download"}>Скачать приложение</Link>
                                     </div>
-                                    <button onClick={() => window.open("https://t.me/m/jidwlHXNNDFi", "_blank")}>Написать нам</button>
+                                    <div className={styles.authButtons}>
+                                        <button >Вход</button>
+                                        <button >Регистрация</button>
+                                    </div>
                                 </div>
                             ) : ""
                         }
